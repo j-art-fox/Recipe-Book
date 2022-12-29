7e6e563d74d0f68528ee6129ef4f24aea9caf08b
Jordan Ring-Sakabe <jordanringsakabe@gmail.com>
2022-12-28 18:00:47 -0700

    added recipes component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { headerComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
 
 @NgModule({
-  declarations: [AppComponent, headerComponent],
+  declarations: [AppComponent, headerComponent, RecipesComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
